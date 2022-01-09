37f2067dbe306768063e69a11d5437c98388d352
btcadman <adman@btcbrochure.com>
2022-01-08 16:07:33 -0800

    Calculate lifetime years

--- components/alwaysOn.tsx
+++ components/alwaysOn.tsx
@@ -4,10 +4,16 @@ import Highlight from "./highlight";
 import PitchPoint from "./pitchPoint";
 
 export default function AlwaysOn() {
+  const millisPerYear = 1000 * 60 * 60 * 24 * 365;
+  const bitcoinBirthday = new Date("2009-01-03");
+  const now = new Date();
+  const lifetimeMillis = now.getTime() - bitcoinBirthday.getTime();
+  const lifetimeYrs = lifetimeMillis / millisPerYear;
+
   return (
     <>
       <PitchPoint title="Always on" slug="always-on">
-        <Highlight>Banking holidays.</Highlight> You know. Those days when your <a className="text-green-500" href="https://www.federalreserve.gov/paymentsystems/fedfunds_about.htm">money</a> doesn&rsquo;t work. The Federal Reserve takes <a href="https://www.federalreserve.gov/aboutthefed/k8.htm">11 days</a> off per year. That&rsquo;s 3% downtime. Add in weekends and it&rsquo;s over 30%. Bitcoin works nights, weekends, and <a href="https://bitcoin.holiday">holidays</a>, with <a href="https://www.reddit.com/r/Bitcoin/comments/gm5m1z/bitcoin_has_a_higher_uptime_than_every_major_tech/">less than 0.03% downtime</a> over its whole lifetime—a 1000x improvement.
+        <Highlight>Banking holidays.</Highlight> You know. Those days when your <a className="text-green-500" href="https://www.federalreserve.gov/paymentsystems/fedfunds_about.htm">money</a> doesn&rsquo;t work. The Federal Reserve takes <a href="https://www.federalreserve.gov/aboutthefed/k8.htm">11 days</a> off per year. That&rsquo;s 3% downtime. Add in weekends and it&rsquo;s over 30%. Bitcoin works nights, weekends, and <a href="https://bitcoin.holiday">holidays</a>, with <a href="https://www.reddit.com/r/Bitcoin/comments/gm5m1z/bitcoin_has_a_higher_uptime_than_every_major_tech/">less than 0.03% downtime</a> over its {Math.floor(lifetimeYrs)} year life—a 1000x improvement.
       </PitchPoint>
 
       <div className="w-full lg:max-w-6xl max-w-xl lg:p-16">
