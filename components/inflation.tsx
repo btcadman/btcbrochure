a5dd3637918f0856117630cee87f20d5273c6248
btcadman <adman@btcbrochure.com>
2022-01-19 11:41:25 -0800

    21 million source

--- components/inflation.tsx
+++ components/inflation.tsx
@@ -5,7 +5,7 @@ import PitchPoint from "./pitchPoint";
 export default function Inflation() {
   return (
     <PitchPoint title="Inflation-proof" slug="inflation">
-      <Highlight>Store your wealth securely.</Highlight> You earned it. Now keep it. Bitcoin has a strictly-limited supply. There will be 21 million bitcoin. That&rsquo;s it.
+      <Highlight>Store your wealth securely.</Highlight> You earned it. Now keep it. Bitcoin has a strictly-limited supply. <a href="https://blog.lopp.net/how-is-the-21-million-bitcoin-cap-defined-and-enforced/">There will be 21 million bitcoin</a>. That&rsquo;s it.
     </PitchPoint>
   )
 }
