d30c01d8d8f4843d011b951e7b810d9350fecd01
btcadman <adman@btcbrochure.com>
2022-01-25 14:30:13 -0800

    Link to Core

--- components/worldwide.tsx
+++ components/worldwide.tsx
@@ -5,7 +5,7 @@ import PitchPoint from "./pitchPoint";
 export default function Worldwide() {
   return (
     <PitchPoint title="Available worldwide" slug="worldwide">
-      <Highlight>Receive Bitcoin transaction data via internet, <a href="https://blockstream.com/satellite/">satellite</a>, even <a href="https://twitter.com/nvk/status/1095354354289135617?s=20">HAM radio</a>.</Highlight>
+      <Highlight>Receive Bitcoin transaction data via internet, <a href="https://blockstream.com/satellite/">satellite</a>, even <a href="https://twitter.com/nvk/status/1095354354289135617?s=20">HAM radio</a>.</Highlight> Anyone can join the Bitcoin network by simply installing <a href="https://github.com/bitcoin/bitcoin">an app</a>.
     </PitchPoint>
   )
 }
