d5a6a2be0ea8f6d4d973c7ee47c0f1f534347f89
btcadman <adman@btcbrochure.com>
2022-01-26 13:05:15 -0800

    Reword

--- components/transfers.tsx
+++ components/transfers.tsx
@@ -5,7 +5,7 @@ import PitchPoint from "./pitchPoint";
 export default function Transfers() {
   return (
     <PitchPoint title="Fast transfers" slug="fast-transfers">
-      <Highlight>Move any amount of money in 10 minutes or less.</Highlight> Compare that to <a href="https://www.marcus.com/us/en/faqs">wire transfer timing, fees, and conditions</a>. No phone calls to confirm. No FBO codes. All you need to send money is Bitcoin wallet software, an <a href="https://river.com/learn/terms/b/bech32-modified/">alphanumeric destination address</a>, and some Bitcoin, of course.
+      <Highlight>Move any amount of money in 10 minutes or less.</Highlight> Compare that to <a href="https://www.marcus.com/us/en/faqs">wire transfer timing, fees, and conditions</a>. No phone calls to confirm. No FBO codes. All you need to send money is Bitcoin wallet software, a <a href="https://river.com/learn/terms/b/bech32-modified/">destination address</a>, and some Bitcoin, of course.
     </PitchPoint>
   )
 }
